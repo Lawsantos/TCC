5cdfc29385215426080a9c68d5672896da055f4c
Lawsantos <laercio.legitimo@gmail.com>
2021-11-05 01:04:16 -0300

    Método cadastrar Usuariovalidação dos formatos de CPF e CEP funcionando e campo senha oculto conforme regras

--- src/main/java/io/github/cwireset/tcc/domain/Usuario.java
+++ src/main/java/io/github/cwireset/tcc/domain/Usuario.java
@@ -1,9 +1,11 @@
 package io.github.cwireset.tcc.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -30,17 +32,18 @@ public class Usuario {
     @NotBlank
     private String email;
     @NotBlank
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String senha;
     @NotBlank
-    @CPF
+    @CPF(message = "O CPF deve ser informado no formato 99999999999.")
     @Pattern(regexp="^\\d{11}$")
-    //@CPF(@Pattern(regexp = " (\"^[0-9]\\d{11}$\")", value = ))
     private String cpf;
     @NotNull
     private LocalDate dataNascimento;
 
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_endereco")
+    @Valid
     private Endereco endereco;
 
     public Usuario(String nome, String email, String senha, String cpf, LocalDate dataNascimento, String cep, String logradouro, String numero, String complemento, String bairro, String cidade, String estado) {
